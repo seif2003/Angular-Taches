de502d29db3efb73eb8aab21d293eb6dec5fda5d
seif2003 <seifddinebenamara2022@gmail.com>
2024-09-30 19:19:21 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TachesComponent } from './taches/taches.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TachesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/taches/taches.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-taches',
+  templateUrl: './taches.component.html',
+  styleUrl: './taches.component.css'
+})
+export class TachesComponent {
+
+}
